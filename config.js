fee17c753e197daced4c593b14f1b37a038f8575
MihailBlack <61623691+MihailBlack@users.noreply.github.com>
2026-03-28 19:03:34 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Конфигурация Google Sheets API
+const CONFIG = {
+    SPREADSHEET_ID: '1ijID-VPJ48qz53gq4MXQTxAxhrFdMuEltm0TY6gwIYQ',
+    SHEET_NAME: 'Sheet1',
+    API_KEY: 'AIzaSyAJr_tsIBBK3COL6AL_8fziqJrKDWDhUVM',
+    RANGE: 'A:D' // A - серийный номер, B - станция, C - дата, D - гарантия
+};
+
+const DEFAULT_WARRANTY_DAYS = 730; // 2 года по умолчанию
